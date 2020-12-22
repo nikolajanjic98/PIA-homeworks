a7d3fae05f08a0d3a17bf372fd14af9caeccecff
nikolajanjic98 <51487394+nikolajanjic98@users.noreply.github.com>
2020-12-22 19:41:13 +0100

    Added quiz questions. Javascript functionality to track correct and incorrect answers. Questions and answers are read from .json file. Added visual response for correct and wrong answers.

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -1,50 +1,222 @@
+var mydata = JSON.parse(data);
+
 const startQuizButton = document.getElementById('startquiz-btn')
 startQuizButton.addEventListener('click', startQuiz)
 
 const mainPage = document.getElementById('WrapperStart')
 const questionPage = document.getElementById('WrapperQuestions')
 
+const score = document.getElementById('Score')
+
 const questionTitle = document.getElementById('QuestionId')
 const questionType = document.getElementById('QuestionType')
 
+const answer1 = document.getElementById('answer1-btn')
+answer1.addEventListener('click', selectAnswer_1)
+const answer2 = document.getElementById('answer2-btn')
+answer2.addEventListener('click', selectAnswer_2)
+const answer3 = document.getElementById('answer3-btn')
+answer3.addEventListener('click', selectAnswer_3)
+const answer4 = document.getElementById('answer4-btn')
+answer4.addEventListener('click', selectAnswer_4)
+
+const nextQuestionButton = document.getElementById('nextquestion-btn')
+nextQuestionButton.addEventListener('click', initNextQuestion)
+
 var currentQuestionIndex = 0
-
-
-
-const questions = [
-    {
-        question: 'Really really really really really really really really really long question name test',
-        type: 'Select the right answer',
-        answers: [
-            { text: 'answer 1', correct: true},
-            { text: 'answer 2', correct: false}
-        ]
-    }
-]
-
-
+var currentCorrectAnswer = -1
+var answered = false
+// score and timer
+var currentScore = 0
 
 function startQuiz()
 {
-    console.log('Quiz started')
     mainPage.classList.add('hide')
     questionPage.classList.remove('hide')
-
-    initNextQuestion(0);
+    initNextQuestion()
 }
 
 function initNextQuestion()
 {
-    showQuestion(questions[currentQuestionIndex])
+    // Reset button states
+    answer1.classList.remove('btn-success')
+    answer1.classList.remove('btn-danger')
+    answer1.classList.remove('btn-outline-info')
+    answer1.classList.add('btn-outline-info')
+
+    answer2.classList.remove('btn-success')
+    answer2.classList.remove('btn-danger')
+    answer2.classList.remove('btn-outline-info')
+    answer2.classList.add('btn-outline-info')
+
+    answer3.classList.remove('btn-success')
+    answer3.classList.remove('btn-danger')
+    answer3.classList.remove('btn-outline-info')
+    answer3.classList.add('btn-outline-info')
+    
+    answer4.classList.remove('btn-success')
+    answer4.classList.remove('btn-danger')
+    answer4.classList.remove('btn-outline-info')
+    answer4.classList.add('btn-outline-info')
+
+    answered = false
+
+    showQuestion(currentQuestionIndex)
+    // take care of max
+    currentQuestionIndex = currentQuestionIndex + 1
 }
 
-function showQuestion(question)
+function showQuestion(index)
 {
-    questionTitle.innerText = question.question
-    questionType.innerText = question.type
+    questionTitle.innerText = mydata[index].question
+    if (mydata[index].type == 'SRA')
+    {
+        questionType.innerText = 'Select right answer.'
+    }
+    answer1.innerText = mydata[index].answer1
+    answer2.innerText = mydata[index].answer2
+    answer3.innerText = mydata[index].answer3
+    answer4.innerText = mydata[index].answer4
+    
+    currentCorrectAnswer = mydata[index].Correct
 }
 
-function selectAnswer()
+function selectAnswer_1()
 {
+    if (answered == false)
+    {
+        if (currentCorrectAnswer == 1)
+        {
+            answer1.classList.remove('btn-outline-info')
+            answer1.classList.add('btn-success')
+            currentScore = currentScore + 1
+            score.innerText = 'Score: ' + currentScore
+        }
+        else
+        {
+            answer1.classList.remove('btn-outline-info')
+            answer1.classList.add('btn-danger')
+            if (currentCorrectAnswer == 2)
+            {
+                answer2.classList.remove('btn-outline-info')
+                answer2.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 3)
+            {
+                answer3.classList.remove('btn-outline-info')
+                answer3.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 4)
+            {
+                answer4.classList.remove('btn-outline-info')
+                answer4.classList.add('btn-outline-success')
+            }
+        }
+        answered = true
+    }
+}
 
+function selectAnswer_2()
+{
+    if (answered == false)
+    {
+        if (currentCorrectAnswer == 2)
+        {
+            answer2.classList.remove('btn-outline-info')
+            answer2.classList.add('btn-success')
+            currentScore = currentScore + 1
+            score.innerText = 'Score: ' + currentScore
+        }
+        else
+        {
+            answer2.classList.remove('btn-outline-info')
+            answer2.classList.add('btn-danger')
+            if (currentCorrectAnswer == 1)
+            {
+                answer1.classList.remove('btn-outline-info')
+                answer1.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 3)
+            {
+                answer3.classList.remove('btn-outline-info')
+                answer3.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 4)
+            {
+                answer4.classList.remove('btn-outline-info')
+                answer4.classList.add('btn-outline-success')
+            }
+        }
+        answered = true
+    }
+}
+
+function selectAnswer_3()
+{
+    if (answered == false)
+    {
+        if (currentCorrectAnswer == 3)
+        {
+            answer3.classList.remove('btn-outline-info')
+            answer3.classList.add('btn-success')
+            currentScore = currentScore + 1
+            score.innerText = 'Score: ' + currentScore
+        }
+        else
+        {
+            answer3.classList.remove('btn-outline-info')
+            answer3.classList.add('btn-danger')
+            if (currentCorrectAnswer == 1)
+            {
+                answer1.classList.remove('btn-outline-info')
+                answer1.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 2)
+            {
+                answer2.classList.remove('btn-outline-info')
+                answer2.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 4)
+            {
+                answer4.classList.remove('btn-outline-info')
+                answer4.classList.add('btn-outline-success')
+            }
+        }
+        answered = true
+    }   
+}
+
+function selectAnswer_4()
+{
+    if (answered == false)
+    {
+        if (currentCorrectAnswer == 4)
+        {
+            answer4.classList.remove('btn-outline-info')
+            answer4.classList.add('btn-success')
+            currentScore = currentScore + 1
+            score.innerText = 'Score: ' + currentScore
+        }
+        else
+        {
+            answer4.classList.remove('btn-outline-info')
+            answer4.classList.add('btn-danger')
+            if (currentCorrectAnswer == 1)
+            {
+                answer1.classList.remove('btn-outline-info')
+                answer1.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 2)
+            {
+                answer2.classList.remove('btn-outline-info')
+                answer2.classList.add('btn-outline-success')
+            }
+            else if (currentCorrectAnswer == 3)
+            {
+                answer3.classList.remove('btn-outline-info')
+                answer3.classList.add('btn-outline-success')
+            }
+        }
+        answered = true
+    }
 }
